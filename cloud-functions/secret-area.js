c094101f1273234186996661b2710e010cf9b9c6
Alena <helen.alisiyevich@gmail.com>
2020-03-19 17:40:36 +1100

    Updated client-area

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,32 +1,40 @@
 exports.handler = function(event, context, callback) {
+    const headers = {
+      "Access-Control-Allow-Origin" : "*",
+      "Access-Control-Allow-Headers" : "Content-Type"
+    }
+   
+    if (event.httpMethod !== "POST") {
+      return callback(null, {
+        statusCode: 200,
+        headers,
+        body: "This was not a POST request"
+      })
+    }
+    
     const secretContent = `
-    <h3> Welcome to the secret area</h3>
-    <p> Now we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+    <h3>Welcome To The Secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
     `
-    let body 
-
+    
+    let body
+   
     if (event.body) {
       body = JSON.parse(event.body)
-    }  else {
+    } else {
       body = {}
     }
-
+   
     if (body.password == "javascript") {
-
-        callback(null, {
-            statusCode: 200,
-            body: secretContent
-        })
-
+      callback(null, {
+        statusCode: 200,
+        headers,
+        body: secretContent
+      })
     } else {
-
-        callback(null, {
-            statusCode: 401
-    
-        })
-
+      callback(null, {
+        statusCode: 401,
+        headers
+      })
     }
-
-
-    
-}
+  }
